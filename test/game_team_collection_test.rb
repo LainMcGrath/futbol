11781424de509f614ffb0cbabb9b372b9ddb2829
Alison Vermeil <alison.minton@gmail.com>
2019-10-25 08:42:10 -0600

    Update test game_team_collection to account for bigger data set and notes on missing tests

--- test/game_team_collection_test.rb
+++ test/game_team_collection_test.rb
@@ -11,9 +11,15 @@ class GameTeamCollectionTest < Minitest::Test
     assert_instance_of GameTeamCollection, @game_team_collection
   end
 
+  #test create_game_teams?
+
   def test_it_has_total_game_team
     @game_team_collection.create_game_teams("./data/game_teams_sample.csv")
-    assert_equal 9, @game_team_collection.total_game_teams.length
+    assert_equal 50, @game_team_collection.total_game_teams.length
+  end
+
+  def test_it_can_find_all_team_ids
+    assert_equal 
   end
 
 end
--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -4,15 +4,26 @@ require './lib/team_collection'
 class TeamsCollectionTest < Minitest::Test
 
   def setup
-    @total_teams = TeamCollection.new("./data/teams_sample.csv")
+    #changed this instance variable name to be consistent with other collection classes
+    @team_collection = TeamCollection.new('./data/teams.csv')
   end
 
   def test_it_exists
-    assert_instance_of TeamCollection, @total_teams
+    assert_instance_of TeamCollection, @team_collection
   end
 
-  def test_it_has_total_teams
-    @total_teams.create_teams('./data/teams_sample.csv')
-    assert_equal 3, @total_teams.total_teams.length
+  # Add test for create_teams?? This could be an argument for making this
+  # a method that later assigns the teams created to the IV?
+
+  def test_it_has_total_teams #it has total teams as an attribute?
+    @team_collection.create_teams('./data/teams.csv')
+    assert_equal 32, @team_collection.total_teams.length
   end
+
+  def test_it_can_get_count_of_teams
+    #this is duplicating testing the @team_collection attribute
+    assert_equal 32, @team_collection.total_teams.length
+  end
+
+  
 end
