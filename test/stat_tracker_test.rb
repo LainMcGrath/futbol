ebbd23ed45eee74cf891ba249d308b81cfde1974
Alison Vermeil <alison.minton@gmail.com>
2019-10-24 19:46:45 -0600

    Add tests for attributes and calculate_highest_total_score method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,6 +1,8 @@
 require './test/test_helper'
 require './lib/stat_tracker'
 require './lib/game_collection'
+require './lib/team_collection'
+require './lib/game_team_collection'
 
 class StatTrackerTest < Minitest::Test
   def setup
@@ -18,6 +20,20 @@ class StatTrackerTest < Minitest::Test
   def test_it_exists
     stat_tracker = StatTracker.new('./data/games_sample.csv', './data/teams.csv', './data/game_teams.csv')
     assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_it_can_be_created_from_csv
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
+  def test_it_has_attributes
+    assert_instance_of GameCollection, @stat_tracker.game_collection
+    assert_instance_of TeamCollection, @stat_tracker.team_collection
+    assert_instance_of GameTeamCollection, @stat_tracker.game_team_collection
+  end
+
+  def test_it_can_calculate_highest_total_score
+    assert_equal 5, @stat_tracker.game_collection.highest_total_score
   end
 
 end
