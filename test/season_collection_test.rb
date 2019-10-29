9e4be6c7a1bdd5ce5c2b6776736e53bd7866461e
Ryan Allen <rallenwasd@gmail.com>
2019-10-29 15:55:49 -0600

    Add test file for season_collection with test_it_has_best_season

---
+++ test/season_collection_test.rb
@@ -0,0 +1,35 @@
+require './test/test_helper'
+require './lib/season_collection.rb'
+require './lib/stat_tracker.rb'
+require './lib/game_collection.rb'
+require './lib/team_collection.rb'
+
+class SeasonCollectionTest < MiniTest::Test
+  def setup
+    game_path = './test/data/games_sample.csv'
+    team_path = './test/data/teams_sample.csv'
+    game_teams_path = './test/data/game_teams_sample.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    games_collection = GameCollection.new(game_path)
+    teams_collection = TeamCollection.new(team_path, game_teams_path)
+    @games = games_collection.total_games
+    @teams = teams_collection.total_teams
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  def test_it_has_best_season
+    assert_equal "20152016", @stat_tracker.best_season("26")
+  end
+
+  def test_it_has_worst_season
+
+  end
+
+  def test_it_has_seasonal_summary
+
+  end
+end
